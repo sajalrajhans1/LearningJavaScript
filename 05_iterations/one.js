3584feda25757fdfff0ca913fcdf5bdff1ba9cdc
Sajal Rajhans <sajalrajhans@gmail.com>
2025-11-27 11:18:28 +0000

    added loop chapters

---
+++ 05_iterations/one.js
@@ -0,0 +1,9 @@
+//for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+// break and continue is used to interrupt loops
+ 
---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// while (condition){ //loop will keep running as long as condition is true.
+//     //statements 
+// }
+
+let i = 0
+while(i <= 10){
+    console.log(`Value of index is ${i}`);
+    i++;
+}
+
+// do{
+//     //statements
+// }while(condition); // does the work first then checks the condition
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
